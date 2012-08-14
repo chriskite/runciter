9a5567e9fa4c272e578530a2f4afc86d5a858e78
Chris Kite <chris@chriskite.com>
2012-08-14 12:45:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Runciter::Application.config.secret_token = 'df9813344f018f030e9dddf8c37993ed7d24d9c5feeb5349b3c4ec82942b9e11db4adf72657f59b5665252f3c888d8d1b78b0d7b46554550c8f756d8bfd2be50'
